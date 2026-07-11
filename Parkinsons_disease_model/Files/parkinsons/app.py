1a24c5356c2fff2414d4b4b1165c78f00a8d7018
FatimaDanah <fbasheer777@gmail.com>
2026-07-11 23:30:21 +0530

    Update app.py

--- Parkinsons_disease_model/Files/parkinsons/app.py
+++ Parkinsons_disease_model/Files/parkinsons/app.py
@@ -1,3 +1,6 @@
+import os
+os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
 from __future__ import annotations
 from datetime import datetime, timezone
 import importlib.util
